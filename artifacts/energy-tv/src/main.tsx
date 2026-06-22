43c87a03e2f4597aabd6284ac37443b7f827cba6
F1FTYCAL <callumschmidt74@gmail.com>
2026-06-22 12:48:23 +1000

    Add Built In Ad-Blocker, Redirect Blocker

--- artifacts/energy-tv/src/main.tsx
+++ artifacts/energy-tv/src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { initAdBlock } from "./lib/adblock";
+
+// Boot ad blocker before any React rendering so fetch/XHR are patched first
+initAdBlock();
 
 createRoot(document.getElementById("root")!).render(<App />);
