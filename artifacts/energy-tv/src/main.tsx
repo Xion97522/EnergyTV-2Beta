8449f00b8a6293b40ea7fa0ca8b17312b490d4c6
EnergyTV <callumschmidt74@gmail.com>
2026-06-24 16:03:00 +1000

    Update main.tsx

--- artifacts/energy-tv/src/main.tsx
+++ artifacts/energy-tv/src/main.tsx
@@ -1,9 +1,14 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { initAdBlock } from "./lib/adblock";
 
-// Boot ad blocker before any React rendering so fetch/XHR are patched first
+// Boot ad blocker BEFORE React renders (important for fetch/XHR patching)
 initAdBlock();
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
